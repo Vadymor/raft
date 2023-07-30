1a911a226fd0461d4ff74045b9ce6dec87f2bf10
Vadym <popikvi@gmail.com>
2023-07-30 21:57:08 +0300

    Fixed log replication at joining

--- server/src/server_api.py
+++ server/src/server_api.py
@@ -1,17 +1,12 @@
 import os
 import time
 import logging as lg
-from datetime import datetime
 
-import aiohttp
 import uvicorn
-from fastapi import FastAPI, BackgroundTasks
-from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
+from fastapi import FastAPI
 from pydantic import BaseModel
-from enum import Enum
+from typing import Optional
 import requests
-import asyncio
 import random
 import threading
 
@@ -30,7 +25,7 @@ HIGH_TIMEOUT = 5000
 REQUESTS_TIMEOUT = 1000
 HB_TIME = 1000
 MAX_LOG_WAIT = 1000
-WAIT_FOR_REPLICATION = 0.0005
+WAIT_FOR_REPLICATION = 5
 
 
 class Node:
@@ -60,7 +55,6 @@ class Node:
         try:
             response = requests.post(destination_url, json=payload, timeout=REQUESTS_TIMEOUT / 1000)
         except Exception as e:
-            print(f"Exception occurred at request {e}")
             return
 
         if response.status_code == 200:
@@ -69,7 +63,7 @@ class Node:
     def initial(self):
         self.set_election_timeout()
 
-        if self.timeout_thread and self.timeout_thread.isAlive():
+        if self.timeout_thread and self.timeout_thread.is_alive():
             return
 
         self.timeout_thread = threading.Thread(target=self.timeout_loop)
@@ -94,17 +88,17 @@ class Node:
         self.role = 'CANDIDATE'
         self.initial()
         self.increment_vote()
-        # TODO send_vote_req
+        self.send_vote_request()
 
     def increment_vote(self):
         self.vote_count += 1
         if self.vote_count >= self.majority:
-            print(f"{self.own_host} is the LEADER on term {self.term}")
+            lg.info(f"{self.own_host} is the LEADER on term {self.term}")
             self.role = 'LEADER'
             self.start_heartbeat()
 
     def start_heartbeat(self):
-        print('Start HB')
+        lg.info('Start HB')
         if self.staged:
             self.handle_post_to_log(self.staged)
 
@@ -113,7 +107,6 @@ class Node:
             thread.start()
 
     def handle_post_to_log(self, payload):
-        print('LOGGING')
 
         self.lock.acquire()
         self.staged = payload
@@ -134,8 +127,8 @@ class Node:
             waited += WAIT_FOR_REPLICATION
             time.sleep(WAIT_FOR_REPLICATION)
 
-            if waited > MAX_LOG_WAIT / 1000:
-                print(f"Waited {MAX_LOG_WAIT} ms, update rejected")
+            if waited > MAX_LOG_WAIT:
+                lg.info(f"Waited {MAX_LOG_WAIT} ms, update rejected")
                 self.lock.release()
                 return False
 
@@ -149,8 +142,12 @@ class Node:
 
         self.commit()
 
+        lg.info(f"This is a commit message: {commit_message}")
+
         threading.Thread(target=self.spread_update,
                          args=(commit_message, None, self.lock)).start()
+
+        return True
 
     def spread_update(self, message, confirmations=None, lock=None):
         for i, each in enumerate(self.others_host):
@@ -180,15 +177,28 @@ class Node:
 
         while self.role == 'LEADER':
             start = time.time()
+
+            lg.info(f"SEND {message}")
             response = self.send_request(follower, route, message)
+
+            message = {'term': self.term, 'leader_id': self.own_host}
             if response:
-                self.heartbeat_response_handler(response)
+                follower_commit_idx = self.heartbeat_response_handler(response)
+
+                if follower_commit_idx < self.commit_idx:
+                    lg.info(f"COMMIT IDX MISMATCH "
+                            f"{follower_commit_idx}, {self.commit_idx}, {len(self.log)}")
+
+                    message['payload'] = self.log[follower_commit_idx]
+                    message['action'] = 'commit'
+                    message['commit_idx'] = self.commit_idx
 
             delta = time.time() - start
 
             time.sleep((HB_TIME - delta) / 1000)
 
     def update_follower_commit_idx(self, follower):
+        lg.info('ENTER update follower')
         route = 'heartbeat'
 
         first_message = {'term': self.term, 'leader_id': self.own_host}
@@ -211,10 +221,15 @@ class Node:
 
         term = hb_response.json()['term']
 
+        lg.info(hb_response.json()['commit_idx'])
+
         if term > self.term:
             self.term = term
             self.role = 'FOLLOWER'
             self.initial()
+        else:
+            commit_idx = hb_response.json()['commit_idx']
+            return commit_idx
 
     def vote_decision(self, term, commit_idx, staged):
 
@@ -225,32 +240,89 @@ class Node:
         else:
             return False, self.term
 
+    def send_vote_request(self):
+        for voter in self.others_host:
+            threading.Thread(target=self.ask_for_vote,
+                             args=(voter, self.term)).start()
+
+    def ask_for_vote(self, voter, term):
+
+        message = {
+            'term': term,
+            'commit_idx': self.commit_idx,
+            'staged': self.staged
+        }
+
+        route = 'vote_request'
+
+        while self.role == 'CANDIDATE' and self.term == term:
+            reply = self.send_request(voter, route, message)
+            if reply:
+                decision = reply.json()['decision']
+                if decision and self.role == 'CANDIDATE':
+                    self.increment_vote()
+                elif not decision:
+                    term = reply.json()['term']
+                    if term > self.term:
+                        self.term = term
+                        self.role = 'FOLLOWER'
+                break
+
+    def handle_get(self, ):
+        lg.info('GET LOG')
+
+        return self.committed_log
+
+    def heartbeat_follower(self, msg):
+        term = msg['term']
+
+        if self.term <= term:
+            self.set_election_timeout()
+
+            if self.role == 'CANDIDATE':
+                self.role = 'FOLLOWER'
+            elif self.role == 'LEADER':
+                self.role = 'FOLLOWER'
+                self.initial()
+            if self.term < term:
+                self.term = term
+
+            if 'action' in msg:
+                lg.info('RECEIVED ACTION')
+
+                action = msg['action']
+
+                if action == 'log':
+                    payload = msg['payload']
+                    self.staged = payload
+                elif self.commit_idx <= msg['commit_idx']:
+                    if not self.staged:
+                        self.staged = msg['payload']
+                    self.commit()
+
+        return self.term, self.commit_idx
+
 
 current_node: Node | None = None
 
 
-class HeartbeatResponse(BaseModel):
-    status: str
-    timestamp: str
-
-
-class Message(BaseModel):
+class VoteMessage(BaseModel):
     term: int
     commit_idx: int
     staged: dict
 
 
-@app.get("/heartbeat", response_model=HeartbeatResponse)
-async def heartbeat():
-    """
-    Heartbeat endpoint to check the API status.
-    """
-    try:
-        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        response_data = HeartbeatResponse(status="OK", timestamp=current_time)
-        return JSONResponse(content=jsonable_encoder(response_data), status_code=200)
-    except Exception as e:
-        lg.info("Internal Server Error")
+class Message(BaseModel):
+    key: str
+    value: str
+
+
+class HeartbeatMessage(BaseModel):
+    term: int
+    leader_id: str
+    payload: Optional[dict] = None
+    action: Optional[str] = None
+    commit_idx: Optional[int] = None
 
 
 @app.on_event('startup')
@@ -266,7 +338,7 @@ async def startup_event():
 
 
 @app.post('/vote_request')
-async def vote_request(message: Message):
+async def vote_request(message: VoteMessage):
     global current_node
     term = message.term
     commit_idx = message.commit_idx
@@ -281,7 +353,48 @@ async def vote_request(message: Message):
 
 @app.get('/message')
 async def get_message():
-    return "HvbtrbtrbrtAL"
+    global current_node
+    result = current_node.handle_get()
+    if result:
+        return {'status': 'success', 'payload': result}
+    else:
+        return {'status': 'fail', 'payload': []}
+
+
+@app.post('/message')
+async def post_message(message: Message):
+    global current_node
+
+    payload = {
+        'key': message.key,
+        'value': message.value
+    }
+
+    result = current_node.handle_post_to_log(payload)
+    if result:
+        return {'status': 'success'}
+    else:
+        return {'status': 'fail'}
+
+
+@app.post('/heartbeat')
+def heartbeat(message: HeartbeatMessage):
+    global current_node
+
+    payload = {
+        'term': message.term,
+        'leader_id': message.leader_id,
+    }
+
+    if message.payload:
+        payload['payload'] = message.payload
+    if message.action:
+        payload['action'] = message.action
+    if message.commit_idx is not None:
+        payload['commit_idx'] = message.commit_idx
+
+    term, commit_idx = current_node.heartbeat_follower(payload)
+    return {"term": term, "commit_idx": commit_idx}
 
 
 if __name__ == '__main__':
